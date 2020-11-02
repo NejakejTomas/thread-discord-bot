964361583ade585e2e718415b27b67418b8de4e4
Tomáš Kiec <kiectoma@fit.cvut.cz>
2020-11-02 02:34:37 +0100

    Add config loading

---
+++ Bot/Config/Config.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Bot.Config
+{
+	sealed class Config : IConfig
+	{
+		[JsonProperty("Bot Token")]
+		public string Token { get; set; } = string.Empty;
+	}
+}
---
+++ Bot/Config/JsonConfig.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using NLog;
+
+namespace Bot.Config
+{
+	static class JsonConfig
+	{
+		private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
+		public static IConfig GetConfig()
+		{
+			const string file = "config.json";
+
+			try
+			{
+				return GetConfig(file);
+			}
+			catch (IOException e)
+			{
+				_logger.Error(e);
+
+				throw;
+			}
+		}
+
+		private static Config GetConfig(string file)
+		{
+			Config? config;
+
+			using (StreamReader fileStream = File.OpenText(file))
+			{
+				JsonReader reader = new JsonTextReader(fileStream);
+				JsonSerializer serializer = new JsonSerializer();
+				config = serializer.Deserialize<Config>(reader);
+			}
+
+			if (config != null) return config;
+
+			throw new IOException($"Error parsing config file {file}");
+		}
+	}
+}
