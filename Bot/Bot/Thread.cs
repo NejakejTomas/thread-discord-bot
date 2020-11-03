e8fd99e7670206553f872eadeab28fa4b77a3101
Tomáš Kiec <kiectoma@fit.cvut.cz>
2020-11-03 10:10:29 +0100

    Add caching

--- Bot/Bot/Thread.cs
+++ Bot/Bot/Thread.cs
@@ -17,7 +17,7 @@ namespace Bot.Bot
 		private const int MaxTextLength = 5500;
 		private const int MaxEmbedsCount = 20;
 		private const int UtilityFieldsCount = 2;
-		private static readonly WeakReferenceContainer<ulong, IUserMessage> wrc = new(100_000);
+		private static readonly WeakReferenceContainer<Tuple<ulong, ulong>, IUserMessage> wrc = new(100_000);
 
 		public static async Task<bool> CreateEmptyAsync(IMessageChannel channel, IUser author, string text = "", ulong replyTo = 0)
 		{
@@ -120,7 +120,14 @@ namespace Bot.Bot
 
 		private static async Task<IUserMessage?> GetThreadMessageAsync(ulong id, IMessageChannel channel)
 		{
-			return await channel.GetMessageAsync(id) as IUserMessage;
+			IUserMessage? thread = wrc.Get(new Tuple<ulong, ulong>(channel.Id, id));
+
+			if (thread != null) return thread;
+
+			thread = await channel.GetMessageAsync(id) as IUserMessage;
+			if (thread != null) wrc.Add(new Tuple<ulong, ulong>(channel.Id, id), thread);
+
+			return thread;
 		}
 
 		private static async Task<IUserMessage> GetLastThreadAsync(IUserMessage threadMessage)
@@ -146,7 +153,7 @@ namespace Bot.Bot
 
 			return int.Parse(footerText[0]);
 		}
-		
+
 		private static async Task SetPrev(IUserMessage threadMessage, string jumpUrl)
 		{
 			EmbedBuilder builder = threadMessage.Embeds.First().ToEmbedBuilder();
